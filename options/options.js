bc285db3681e4464f9e36678b2bdc90373b1f66f
daniel palacios <daniel.palacios@crowdbotics.com>
2023-05-26 15:50:52 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "testdanp2023052525--86143",
-  url: "https://testdanp2023052525--86143.botics.co",
-  api: "https://testdanp2023052525--86143.botics.co/api/v1"
-}
+  "name": "testdanp2023052525--86143",
+  "url": "https://testdanp2023052525--86143.botics.co",
+  "api": "https://testdanp2023052525--86143.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled2/index.js
@@ -0,0 +1,27 @@
+import { TextInput } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled2 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><TextInput style={styles.EyYfbBsG}></TextInput></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  EyYfbBsG: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  }
+});
+export default Untitled2;
